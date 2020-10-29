a12113a106f585d9c46cd4d8c99def5dd3be3591
李杰 <lijie@lijiedeMacBook-Pro.local>
2020-10-29 13:16:43 +0800

    f

--- package/os/stat/main.go
+++ package/os/stat/main.go
@@ -1,3 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
 
+func example(file string) {
+	fileInfo,err := os.Stat(file)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("name:",fileInfo.Name())
+	fmt.Println("isdir:",fileInfo.IsDir())
+	fmt.Println("mode:",fileInfo.Mode())
+	fmt.Println("modTime:",fileInfo.ModTime())
+	fmt.Println("size:",fileInfo.Size())
+	fmt.Println("sys:",fileInfo.Sys())
+}
+
+func main() {
+	file := "./a.txt"
+	example(file)
+}
